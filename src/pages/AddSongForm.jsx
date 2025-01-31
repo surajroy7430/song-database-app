3f556ecbbcbe76903adc32832baea290b2e5e63e
surajroy7430 <surajroy7430@gmail.com>
2025-01-31 12:19:08 +0530

    some edit

--- src/pages/AddSongForm.jsx
+++ src/pages/AddSongForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { toast } from 'react-toastify';
-import { db, collection, addDoc, updateDoc } from "../firebase/firebase-config";
+import { db, collection, addDoc, updateDoc } from "../firebase";
 import useSong from "../hooks/useSong";
 
 const AddSongForm = () => {
